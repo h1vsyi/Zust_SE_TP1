caa86f0443ba6530eb514fe8cdb7ae883ca757d3
zuowanxiao <138018353+zuowanxiao@users.noreply.github.com>
2023-06-28 21:13:25 +0800

    Exception specified

--- TP1/src/main/java/dividebyzero/Main.java
+++ TP1/src/main/java/dividebyzero/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             System.out.println(3 / 0);
-        }catch (Exception e){
+        } catch (ArithmeticException e) {
             System.err.println(e);
         }
     }
