726991ffc5e4307b80783f3f8dadad1c7ff8cddf
h1vsyi <2863168526@qq.com>
2023-06-28 20:46:44 +0800

    try/catch block added

--- TP1/src/main/java/dividebyzero/Main.java
+++ TP1/src/main/java/dividebyzero/Main.java
@@ -5,7 +5,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(3/0);
+        try {
+            System.out.println(3 / 0);
+        }catch (Exception e){
+            System.err.println(e);
+        }
     }
 
 }
